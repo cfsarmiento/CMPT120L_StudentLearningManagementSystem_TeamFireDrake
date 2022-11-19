451dda4489d9663b9f1c9d08dfcf95d1e0b151bb
gabrielle-knapp1 <116968433+gabrielle-knapp1@users.noreply.github.com>
2022-11-19 17:57:23 -0500

    Add files via upload

--- finalized gui/main page project mike.py
+++ finalized gui/main page project mike.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import *
 window=tk.Tk()
 window.title("Main Page")
-window.geometry("1050x550")
+window.geometry("1550x550")
 window.config(bg="Gray")
 
 frame1=tk.Frame(window,bg="Gray",highlightbackground="White",highlightthickness=1,width=100,height=100)
@@ -17,18 +17,23 @@ label= tk.Label(bg="Gray", fg="White", text="Semester GPA:    \nCummulative GPA:
 
 frame3=tk.Frame(window,bg="Gray",width=249,height=249)
 frame3.grid(row=1,column=0)
-tk.Button(frame3,bg="Gray",fg="White",text="Cummulative Rundown",font='Helvetica 12 bold').grid(row=0,column=0,pady=10)
-tk.Button(frame3,bg="Gray",fg="White",text="Potential GPA Calc",font='Helvetica 12 bold').grid(row=1,column=0)
+
 num=1
-for i in range(1,6):
+for i in range(1,9):
     tk.Button(frame3,bg="Gray",fg="White",text="Class "+str(num),font='Helvetica 12 bold').grid(row=3+num,column=0,pady=5)
     num+=1
-tk.Button(frame3,bg="Gray",fg="White",text="Settings",font='Helvetica 12 bold').grid(row=9, column=0)
-tk.Button(frame3,bg="Gray",fg="White",text="Add/Edit Grade",font='Helvetica 12 bold').grid(row=8,column=0)
 
 frame4=tk.Frame(window,bg="Gray",highlightbackground="White",highlightthickness=1,width=500,height=300)
 frame4.grid(row=1,column=1)
 textBox1=Text(frame4, bg="Gray", fg="White").grid(row=1, column=0)
-frame4.mainloop()
+
+
+frame5=tk.Frame(window,bg="Gray", width=500,height=300)
+frame5.grid(row=1, column=2)
+tk.Button(frame5,bg="Gray",fg="White",text="Add Current Semester",font='Helvetica 12 bold').grid(row=0,column=2, pady=10)
+tk.Button(frame5,bg="Gray",fg="White",text="Add Previous Semester",font='Helvetica 12 bold').grid(row=1, column=2, pady=5)
+tk.Button(frame5,bg="Gray",fg="White",text="Adjust Current Semester",font='Helvetica 12 bold').grid(row=2,column=2, pady=10)
+tk.Button(frame5,bg="Gray",fg="White",text="Cummulative Rundown",font='Helvetica 12 bold').grid(row=3,column=2,pady=10)
+tk.Button(frame5,bg="Gray",fg="White",text="Potential GPA Calc",font='Helvetica 12 bold').grid(row=4,column=2)
 
 window.mainloop()
