75d11eff4d7cbd217948ad6d241fd2eb2bf1a8a4
Ethan Morton <116925906+ethanMortonMarist@users.noreply.github.com>
2022-11-19 23:24:26 -0500

    couple of changes and comments for future plans

--- Source_Code/mainPage.py
+++ Source_Code/mainPage.py
@@ -1,11 +1,18 @@
 '''
-Authors: Michael McMahon and Gabrielle Knapp
-Title: Main Page
-Goal: Print out a main page and to create buttons that direct to other pages
-OR: Use tkinter for GUI
+Title: Add Class
+Window: Add Class Window
+Author: Ethan Morton
+Class: CMPT120L
+Professor: Reza Sadeghi
+Goal: 
+Widgets: 
+Other Requirements: TKInter for Interface Design.
 '''
 import tkinter as tk
 from tkinter import *
+import os
+import pathlib
+#os.chdir(pathlib.Path(__file__).parent.resolve())  # we need to create a folder for each semester so that I can use that directory to open the files
 window=tk.Tk()
 window.title("Main Page")
 window.geometry("1550x550")
@@ -20,19 +27,35 @@ frame2.grid(row=0,column=1)
 label= tk.Label(bg="Gray", fg="White", text="Course Name:       Grade:       Status:       ", font='Helvetica 12 bold').grid(row=0,column=1)
 label= tk.Label(bg="Gray", fg="White", text="Semester GPA:    \nCummulative GPA:     ", font='Helvetica 12 bold').grid(row=0,column=2)
 
-
 frame3=tk.Frame(window,bg="Gray",width=249,height=249)
 frame3.grid(row=1,column=0)
 
-num=1
-for i in range(1,9):
-    tk.Button(frame3,bg="Gray",fg="White",text="Class "+str(num),font='Helvetica 12 bold').grid(row=3+num,column=0,pady=5)
-    num+=1
+#num=1
+for i in range(1,9):    # instead of having range(1,9), this will get the number of classes and iterate through that. range(1,9) is just a placeholder
+    tk.Button(frame3,bg="Gray",fg="White",text="Class "+str(i),font='Helvetica 12 bold').grid(row=3+i,column=0,pady=5)
+    #num+=1
 
 frame4=tk.Frame(window,bg="Gray",highlightbackground="White",highlightthickness=1,width=500,height=300)
 frame4.grid(row=1,column=1)
-textBox1=Text(frame4, bg="Gray", fg="White").grid(row=1, column=0)
+#textBox1=Text(frame4, bg="Gray", fg="White").grid(row=1, column=0)
+yearFrame = tk.Frame(frame4, bg = "Gray", width = 166, height = 300)
+semesterFrame = tk.Frame(frame4, bg = "Gray", width = 166, height = 300)
+gpaFrame = tk.Frame(frame4, bg = "Gray", width = 166, height = 300)
 
+yearLabel = tk.Label(yearFrame, bg = "Gray", fg = "White", text = "Year", font = 'Helvetica 12 bold')
+yearLabel.grid(row=0,column=0)
+
+semesterLabel = tk.Label(semesterFrame, bg = "Gray", fg = "White", text = "Semester", font = 'Helvetica 12 bold')
+semesterLabel.grid(row=0,column=0)
+
+gpaLabel = tk.Label(gpaFrame, bg = "Gray", fg = "White", text = "GPA", font = 'Helvetica 12 bold')
+gpaLabel.grid(row=0,column=0)
+
+'''
+Each semester should have its own folder, so I need to iterate through each folder
+Within each folder, there should be a file with the semester information: year, session, GPA
+They should be sorted in order of most recent at the top to oldest at the bottom
+'''
 
 frame5=tk.Frame(window,bg="Gray", width=500,height=300)
 frame5.grid(row=1, column=2)
