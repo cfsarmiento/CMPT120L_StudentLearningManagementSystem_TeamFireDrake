8f123c5b80d0feb8d518f770c20d33b43c47d8df
Ethan Morton <116925906+ethanMortonMarist@users.noreply.github.com>
2022-11-20 19:59:23 -0500

    quick fix

--- Source_Code/mainPage.py
+++ Source_Code/mainPage.py
@@ -137,7 +137,7 @@ def MAIN_PAGE():
     for i, file in enumerate(os.listdir(os.path.join(os.getcwd(), mostRecentSemesterFile)), start = 1):
         if (os.path.isfile(os.path.join(os.getcwd(), mostRecentSemesterFile, file)) and file != "semesterInfo.csv"):
             course = file[file.index("e")+1:file.index(".")]
-            tk.Button(frame3,bg="Gray",fg="White",text=course+".csv",font='Helvetica 12 bold', command=lambda: Class(course)).grid(row=3+i,column=0,pady=5)
+            tk.Button(frame3,bg="Gray",fg="White",text=course,font='Helvetica 12 bold', command=lambda: Class(course)).grid(row=3+i,column=0,pady=5)
 
     frame4=tk.Frame(window,bg="Gray",highlightbackground="White",highlightthickness=1,width=500,height=100)
     frame4.grid(row=1,column=1)
