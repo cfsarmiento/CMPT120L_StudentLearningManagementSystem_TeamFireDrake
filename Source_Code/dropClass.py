7788ca2050978d8b991a9d33ffcd8d2bd0d7fb4b
Ethan Morton <116925906+ethanMortonMarist@users.noreply.github.com>
2022-11-22 09:38:15 -0500

    drop class works now

--- Source_Code/dropClass.py
+++ Source_Code/dropClass.py
@@ -1,7 +1,7 @@
 '''
 Title: Student Learning Management System
 Window: Drop/Withdrawl Window
-Authors: Christian Sarmiento and Gabrielle Knapp
+Authors: Ethan Morton
 Class: CMPT120L
 Professor: Reza Sadeghi
 Goal: Allows user to drop or withdraw from a class they are currently enrolled and taking
@@ -14,12 +14,23 @@ def DROP_WITHDRAWL():
     import sourceCodeLibrary
     mostRecentSemesterDirectory = sourceCodeLibrary.GetMostRecentSemesterDirectory()
     os.chdir(mostRecentSemesterDirectory)
+
+    def Search():
+        course = searchEntry.get()
+        classFoundLabel.configure(text=course+" not found")
+        for file in os.listdir(mostRecentSemesterDirectory):
+            if (course in file):
+                classFoundLabel.configure(text=course+" found")
     
-    def Drop(file):
-        os.remove(file)
-        drop_withdrawl.destroy()
-        import adjustCurrentSemester
-        adjustCurrentSemester.ADJUST_CURRENT_SEMESTER()
+    def Drop():
+        course=searchEntry.get()
+        try:
+            os.remove("course"+course+".csv")
+            drop_withdrawl.destroy()
+            import adjustCurrentSemester
+            adjustCurrentSemester.ADJUST_CURRENT_SEMESTER()
+        except:
+            classFoundLabel.configure(text="Could not drop "+course)
 
     def Back():
         drop_withdrawl.destroy()
@@ -29,35 +40,28 @@ def DROP_WITHDRAWL():
     # Window
     drop_withdrawl = tk.Tk()
     drop_withdrawl.title('Drop')  # title for window
-    drop_withdrawl.geometry('300x300')  # length x width
+    drop_withdrawl.geometry('450x300')  # length x width
     drop_withdrawl.configure(bg = 'grey')  # background color
 
-    # Frames
-    classSelectionFrame = tk.Frame(drop_withdrawl, bg = 'grey', width = 200, height = 100)
-    dropWithdrawlFrame = tk.Frame(drop_withdrawl, bg = 'grey' , height = 50, width = 300)
+    searchFrame = tk.Frame(drop_withdrawl,bg = "Gray")
+    searchFrame.grid(row=0, column=0)
 
-    classSelectionFrame.grid(row = 1, column = 5)
-    dropWithdrawlFrame.grid(row = 2, column = 5)
-    # Label for Select a Class
-    selectClassLabel = tk.Label(drop_withdrawl,
-                                text = 'Select a Class: ',
-                                bg = 'grey',
-                                fg = 'white',
-                                font = 'Helvetica 12 bold',
-                                padx = 30,
-                                pady = 30)
-    selectClassLabel.grid(row = 0, column = 5)
+    searchLabel = tk.Label(searchFrame, bg = "Gray", fg = "White", text = "Search the class you want to drop:", font = 'Helvetica 12 bold')
+    searchLabel.grid(row = 0, column = 0)
+    searchEntry = tk.Entry(searchFrame, bg = 'Gray')
+    searchEntry.grid(row = 1, column = 0)
 
-    classButtons = list[tk.Button]()
-    classFiles = list[str]()
-    for i, file in enumerate(os.listdir(mostRecentSemesterDirectory)):
-        if ("course" in file):
-            course = file[file.index("e")+1:file.index(".")]
-            classFiles.append(file)
-            classButtons.append(tk.Button(classSelectionFrame,bg="grey",fg="white",text="Drop "+course,font='Helvetica 12 bold', command=lambda: Drop(classFiles[classFiles.index("course"+course+".csv")])).grid(row = i, column = 1))
+    searchButton = tk.Button(drop_withdrawl,bg="grey",fg="white",text="Search",font='Helvetica 12 bold', command=Search)
+    searchButton.grid(row = 0, column = 1)
+
+    classFoundLabel = tk.Label(drop_withdrawl, bg = "Gray", fg = "White", text = "", font = 'Helvetica 12 bold')
+    classFoundLabel.grid(row = 1, column = 0)
+
+    dropButton = tk.Button(drop_withdrawl,bg="grey",fg="white",text="Drop this course",font='Helvetica 12 bold', command= Drop)
+    dropButton.grid(row = 2, column = 0)
 
     # Back Button
     btnBack = tk.Button(drop_withdrawl, text = 'Back', fg='white', bg='grey', font='Helvetica 12 bold', command=Back)
-    btnBack.grid(row = 0, column = 0)
+    btnBack.grid(row = 3, column = 0)
 
     drop_withdrawl.mainloop()
