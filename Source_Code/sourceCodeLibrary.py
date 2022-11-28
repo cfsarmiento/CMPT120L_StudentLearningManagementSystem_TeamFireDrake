8279d4d7020f6dd3a5ad522b75065ee5e167bd02
Ethan Morton <116925906+ethanMortonMarist@users.noreply.github.com>
2022-11-28 14:05:48 -0500

    quick fix

--- Source_Code/sourceCodeLibrary.py
+++ Source_Code/sourceCodeLibrary.py
@@ -1,21 +1,29 @@
 '''
+Title: Source Code Library
+Author: Ethan Morton
+Class: CMPT120L
+Professor: Reza Sadeghi
+Goal: This is a library for certain functions that will be used multiple times throughout the source code. This prevents repetition and redundancy.
+Other Requirements: none.
 '''
+# calculates grades, GPA, and updates the GUI
 def CalculateCourseGrade():
-    # to be called in add/edit/remove Assignment.py where indicated by the #comment
     import os
     import pathlib
     import csv
-
+    # this will be called in add/edit/removeAssignment.py where indicated by the #comment
+    # this function may need to take an argument to get the name of the course
+    # 1)
     # reads through course___.csv of this course and calculates the course grade
     # writes this course grade back into course___.csv
-    
-    # reads through course___.csv of all courses and calculates semester GPA
+    # 2)
+    # reads through course___.csv of all courses in this semester and calculates semester GPA
     # writes semester GPA in semesterInfo.csv in this semester
-
+    # 3)
     # reads through semesterInfo.csv of all semesters and calculates cumulative GPA
-    # writes the cumulative GPA on the main page
-    pass
+    # writes the cumulative GPA in the mainPage.py
 
+# returns the directory of the most recent semester
 def GetMostRecentSemesterDirectory():
     import os
     import pathlib
@@ -27,11 +35,10 @@ def GetMostRecentSemesterDirectory():
         for line in reader:
             accountFile = line[0]
     os.chdir(os.path.join(pathlib.Path(__file__).parent.parent, "Accounts", accountFile, "Semesters"))
-    semesters = os.listdir(os.getcwd())
     mostRecentSemesterFile = ""
     mostRecentYear = 0
     mostRecentSession = 0
-    for semester in semesters:
+    for semester in os.listdir(os.getcwd()):
         if os.path.isdir(os.path.join(os.getcwd(), semester)):
             year = int(semester[semester.index("_")+1:semester.rindex("_")])
             session = 0
